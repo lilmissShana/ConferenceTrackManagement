85fea6bb6c21c7bafbf01e8364428f94aad99439
Shana Dacres <shana.dacres@capgemini.com>
2018-06-20 00:00:29 +0100

    added functionality to read from file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,62 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+    static ArrayList<Talk> talkList = new ArrayList<Talk>();
 
-    static String time_regex = ".*[1-9]min";
-    // reads in a list of talks and print out the track lists
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Talk> talkList = new ArrayList<Talk>();
-        boolean hasNext = true;
-        while (hasNext) {
-            Talk talk;
-            String line = scanner.nextLine();
-            line = line.trim();
-            if (line.isEmpty()) {
-                hasNext = false;
-                break;
-            }
-            else {
-                if (line.contains("lightning")) {
-                    talk = new Talk(line, 5);
-                    talkList.add(talk);
+        // if a file is given at run time read it
+        if (args != null) {
+            String file_name = args[0].trim();
+            File file = new File(file_name);
 
-                } else if (line.contains("min")) {
+            if (file.exists()) {
+                try {
+                    String line = "";
+                    BufferedReader b_reader = new BufferedReader(new FileReader(file));
+                    while ((line = b_reader.readLine()) != null)
+                    {
+                        line = line.trim();
+                        organiseTalk(line);
+                    }
 
-                    String title = line.substring(0, line.length() - 5);
-                    title = title.trim();
-                    int time =
-                            Integer.parseInt(line.substring(title.length(), line.length() - 3));
-                    talk = new Talk(title, time);
-                    talkList.add(talk);
+                } catch (IOException e) {
+                    throw new Exception("Could not read file - " + file.getPath());
 
-                } else {
-                    System.out.println("cant organise talk - " + line);
                 }
+                // call the below function
+            } else {
+                throw new Exception("File cant be found - " + file.getPath());
             }
-
         }
-        scanner.close();
+        // else read from terminal
+        else {
+
+            Scanner scanner = new Scanner(System.in);
+
+            boolean hasNext = true;
+            while (hasNext) {
+
+                String line = scanner.nextLine();
+                line = line.trim();
+                if (line.isEmpty()) {
+                    hasNext = false;
+                    break;
+                } else {
+                    organiseTalk(line);
+                }
+
+            }
+            scanner.close();
+        }
 
         Conference conference = new Conference();
 
@@ -59,6 +74,28 @@ public class Main {
             }
         }
 
+
     }
+
+    public static void organiseTalk(String line) throws Exception {
+        Talk talk;
+
+        if (line.contains("lightning")) {
+            talk = new Talk(line, 5);
+            talkList.add(talk);
+
+        } else if (line.contains("min")) {
+
+            String title = line.substring(0, line.length() - 5);
+            title = title.trim();
+            int time = Integer.parseInt(line.substring(title.length(), line.length() - 3).trim());
+            talk = new Talk(title, time);
+            talkList.add(talk);
+
+        } else {
+            throw new Exception("Cant organise talk - " + line);
+        }
+    }
+
 
 }
